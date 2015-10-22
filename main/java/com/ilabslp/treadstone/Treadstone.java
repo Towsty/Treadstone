9ead06bb2833a8f4e98d4dda9312b4157fbd9228
Towsty <towstyg2523@gmail.com>
2015-10-22 02:18:27 -0500

    Initial Commit Proxy Class derp - wont launch

---
+++ main/java/com/ilabslp/treadstone/Treadstone.java
@@ -0,0 +1,34 @@
+package com.ilabslp.treadstone;
+
+import com.ilabslp.treadstone.core.proxy.CommonProxy;
+import com.ilabslp.treadstone.lib.References;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = References.MODID, name = References.MODNAME, version = References.VERSION)
+public class Treadstone {
+    @SidedProxy(clientSide = References.CLIENTPROXYLOCATION, serverSide = References.COMMONPROXYLOCATION)
+    public static CommonProxy proxy;
+    
+	@Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event)
+    {
+ 
+    }
+ 
+    @Mod.EventHandler
+    public static void init(FMLInitializationEvent event)
+    {
+ 
+    }
+ 
+    @Mod.EventHandler
+    public static void postInit(FMLPostInitializationEvent event)
+    {
+ 
+    } 
+}
---
+++ main/java/com/ilabslp/treadstone/core/proxy/CommonProxy.java
@@ -0,0 +1,5 @@
+package com.ilabslp.treadstone.core.proxy;
+
+public class CommonProxy {
+
+}
